057fea0590fa2197be0c0b887200ad76043ea7fd
zvezdochey <trofimov06@icloud.com>
2024-11-14 19:20:24 +0300

    Update helloworld.go

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -1,7 +1,7 @@
-package helloworld
+package HelloWorld
 
 // This is your first homework!
 // Take a look at tests to get an idea of what this function should do.
 func HelloWorld() string {
-	return ""
+	return "Hello, World!"
 }
